24f125aebe12aba13ddd3aac889bb414a168caed
AlaeddineBenouadah <mabenouadah@gmail.com>
2025-11-13 15:37:36 +0100

    feat: nouvelle fonctionnalité V2 et mise à jour de la version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Application Node.js déployée via CI/CD Docker !</h1>');
+  res.send('<h1>Application Node.js V2.0 : Déploiement via Pull Request réussi !</h1>');
 });
 
 app.listen(port, () => {
